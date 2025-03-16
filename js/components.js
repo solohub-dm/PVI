c586e9bed4daa41d9404fe2c8589cf1d92312cdb
solohub-dm <solohub.dm@gmail.com>
2025-03-16 23:53:01 +0200

    adaptive no-windows

--- js/components.js
+++ js/components.js
@@ -1,32 +1,62 @@
-function includeHTML(file, elementId, callback) {
-  fetch(file)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById(elementId).innerHTML = data;
-      if (callback) callback();
-    })
-    .catch((error) => console.error('Error loading ${file}:', error));
+function includeHTML(file, elementId) {
+  return new Promise((resolve, reject) => {
+    fetch(file)
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById(elementId).innerHTML = data;
+        resolve();
+      })
+      .catch((error) => {
+        console.error(`Error loading ${file}:`, error);
+        reject(error);
+      });
+  });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  includeHTML("./components/header.html", "header-placeholder", () => {
+function loadScript(scriptSrc) {
+  return new Promise((resolve, reject) => {
     const script = document.createElement("script");
-    script.src = "./js/main.js";
+    script.src = scriptSrc;
     script.type = "text/javascript";
+    script.onload = resolve;
+    script.onerror = reject;
     document.head.appendChild(script);
   });
-  includeHTML("./components/sidebar.html", "sidebar-placeholder", () => {
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+
+    await includeHTML("./components/header.html", "header-placeholder");
+    await includeHTML("./components/sidebar.html", "sidebar-placeholder");
+
     let currentPath = window.location.pathname.split("/").pop();
     const navItems = document.querySelectorAll(".sidebar-menu-item");
-  
-      if (navItems.length > 0) {
-        navItems.forEach((link) => {
-          link.getAttribute("href") === "./" + currentPath
-            ? link.classList.add("active")
-            : link.classList.remove("active");
-        });
+
+    navItems.forEach((link) => {
+      link.getAttribute("href") === "./" + currentPath
+        ? link.classList.add("active")
+        : link.classList.remove("active");
+    });
+
+    await loadScript("./js/main.js");
+
+    if (currentPath === "students.html") {
+      await loadScript("./js/table.js");
+    }
+    
+      const isOpen = localStorage.getItem("menuOpen") === "true";
+      console.log("isOpen: " + isOpen);
+      if (isOpen) {
+        sidebarWrapper.classList.add("no-transition");
+        openSidebar();
+        setTimeout(() => {
+          sidebarWrapper.classList.remove("no-transition");
+        }, 50);
       }
-  });
 
 
-}); 
+  } catch (error) {
+    console.error("Error during initialization:", error);
+  }
+});
